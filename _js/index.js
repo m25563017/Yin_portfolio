596f8fd33fdf55d93a5f7e6b43a2cf1a0458d817
m25563017 <71679886+m25563017@users.noreply.github.com>
2023-03-10 09:09:31 +0800

    第一次更新

---
+++ _js/index.js
@@ -0,0 +1,23 @@
+//宣告預設表單內容為空
+var initSubject = '', initBody = '';
+
+//按下傳送按鈕後執行
+function submitHandler() {
+    var to = "m25563017@gmail.com";//寫死的傳送對象 就是公司的信箱 不會顯示在網頁上
+    var name = nameText.value;//讀取ID為 nameTextuser 物件中的值
+    var email = emailText.value;
+    var subject = subText.value;
+    //把user填的資料都塞到 mail body 中
+    var body = "" + bodyText.value + '%0A%0A%0A';//%0A是換行 換了三行
+    body += "From：" + nameText.value + '%0A';
+    body += "Email：" + emailText.value + '%0A';
+    //傳送的主要程式碼
+    mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
+    mailTo.click();
+}
+//在body onload
+function init() {
+    subText.value = initSubject;
+    toText.value = initTo;
+    bodyText.value = initBody;
+}
